9494b0ec6356598562e05af56f622cd5653f51c5
Kayden Ong Hao Cong <kayden.ong@mail2.stengg.com>
2022-04-01 17:30:43 +0800

    laptop 2nd commit

--- lib/ApiConstants.dart
+++ lib/ApiConstants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   static String baseUrl = 'https://api.twilio.com/2010-04-01/Accounts/ACb754fdd9671d70fa05d11c04fba63e2b/Messages.json';
   static String usersEndpoint = '/users';
-  static String authToken = '';
+  static String authToken = '832469bca9daa781d70d9f5a9d829beb';
   
   static String to = '+6598326613';
   static String from = '+17579796397';
